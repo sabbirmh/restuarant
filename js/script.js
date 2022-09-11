972ff9114001d58cef4d5214978890a296dff711
sabbirmh <sabbirmh92@gmail.com>
2022-09-11 21:33:57 +0600

    first

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    'use strict';
+    // chef js
+
+
+    $('.slider_row').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:false,
+      });
+
+
+});
